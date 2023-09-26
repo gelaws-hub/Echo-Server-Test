57dcd250ad1867da83c925911a79e351ed2557ab
Ibnu Fadhil <73326654+gelaws-hub@users.noreply.github.com>
2023-09-26 18:47:25 +0700

    Update ForkEchoServer.c

--- Tugas4/ForkEchoServer.c
+++ Tugas4/ForkEchoServer.c
@@ -6,14 +6,13 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
 
 int main() {
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(3000);
-    server_addr.sin_addr.s_addr = inet_addr("192.168.0.102"); // Menggunakan alamat IP server
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     int sockfd;
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -22,28 +21,64 @@ int main() {
     }
     printf("Socket created\n");
 
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Couldn't connect");
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Couldn't bind socket");
         return 1;
     }
-    printf("Connected to the server\n");
+    printf("Bound to port 3000\n");
 
-    char msg[100], server_msg[100];
+    if (listen(sockfd, 5) < 0) {
+        perror("Couldn't listen to socket");
+        return 1;
+    }
+    printf("Listening for connections (max backlog: 5)\n");
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
+
     while (1) {
-        printf("Write message : ");
-        scanf("%99[^\n]%*c", msg); // Batasi input pesan maksimal menjadi 99 karakter
-        send(sockfd, msg, strlen(msg), 0); // Mengirim pesan sesuai panjang pesan yang sebenarnya
-        memset(server_msg, 0, sizeof(server_msg));
-        ssize_t bytes_received = recv(sockfd, server_msg, sizeof(server_msg), 0);
-        if (bytes_received <= 0) {
-            perror("Connection closed or error occurred");
-            break;
+        int client_sockfd;
+        if ((client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_size)) < 0) {
+            perror("Couldn't accept connection");
+            return 1;
         }
-        printf("Server reply : %s\n", server_msg);
-        if (strcmp(server_msg, "bye") == 0) {
-            printf("Exiting...\n");
-            break;
+        printf("Accepted connection from %s\n", inet_ntoa(client_addr.sin_addr));
+
+        pid_t pid = fork(); // Membuat proses anak
+        if (pid == 0) { // Kode proses anak
+            close(sockfd); // Tutup socket di proses anak
+            char msg[100];
+
+            while (1) {
+                ssize_t bytes_received = recv(client_sockfd, msg, sizeof(msg), 0);
+                if (bytes_received <= 0) {
+                    printf("Connection closed or error occurred\n");
+                    break;
+                }
+                msg[bytes_received] = '\0'; // Tambahkan null-terminator
+
+                printf("Received message: %s\n", msg);
+
+                ssize_t bytes_sent = send(client_sockfd, msg, bytes_received, 0);
+                if (bytes_sent < 0) {
+                    perror("Error sending message");
+                    break;
+                }
+
+                if (strcmp(msg, "bye") == 0) {
+                    printf("Exiting...\n");
+                    break;
+                }
+            }
+
+            close(client_sockfd);
+            exit(0); // Keluar dari proses anak
+        } else if (pid < 0) {
+            perror("Couldn't create child process");
+            return 1;
         }
+
+        close(client_sockfd); // Tutup socket di proses utama (parent)
     }
 
     close(sockfd);
